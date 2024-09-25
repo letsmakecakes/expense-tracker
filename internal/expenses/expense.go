f91467510663111501c8608c9051d46f32180e62
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-09-25 23:00:43 +0530

    added expense struct model

--- internal/expenses/expense.go
+++ internal/expenses/expense.go
@@ -1,1 +1,10 @@
 package expenses
+
+import "time"
+
+type Expense struct {
+	ID          int       `json:"id"`
+	Date        time.Time `json:"date"`
+	Description string    `json:"description"`
+	Amount      int       `json:"amount"`
+}
