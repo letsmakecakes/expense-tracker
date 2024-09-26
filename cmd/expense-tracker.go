85ee3ae11ee9f5a9a13f0364c92823a5d2f0c868
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-09-26 22:01:32 +0530

    added main program

--- cmd/expense-tracker.go
+++ cmd/expense-tracker.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"expense-tracker/pkg/cli"
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Print("expected a command (add, list, summary, delete)")
+	}
+
+	cli.HandleCommand(os.Args[1:])
+}
